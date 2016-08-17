2e6ed7193fd2b0e05d27d97cd61f25f635f73123
Jack Pien (Felton) <jack.pien@eeme.co>
2016-08-17 07:58:32 -0700

    Rosbots init waits for IP

--- tools/rpi_setup/rosbots_service_template.bash
+++ tools/rpi_setup/rosbots_service_template.bash
@@ -4,34 +4,16 @@
 # Provides:		rosbots
 # Required-Start:	$all
 # Required-Stop:	$remote_fs $syslog
-# Default-Start:	2 3 4 5
+# Default-Start:	5
 # Default-Stop:		0 1 6
 # Short-Description:	ROSCORE and other ros launch scripts
 ### END INIT INFO
 
-# Try to get wifi address first
-my_ip="$(ifconfig wlan0 | grep inet | grep -v inet6 | awk '{print $2}' | sed 's/addr://g')"
-
-if  [ "${my_ip}" == "" ]; then
-    my_ip="$(ifconfig eth0 | grep inet | grep -v inet6 | awk '{print $2}' | sed 's/addr://g')"
-fi
-
 export ROSBOTS_HOME=_TEMPLATE_HOME
 export ROSBOTS_WS_PATH=_TEMPLATE_WS_PATH
-. ${ROSBOTS_WS_PATH}/build/opt/ros/kinetic/setup.sh
-
-export ROS_IP=${my_ip}
-
-# Add some more paths to make ros work
-export PATH=${PATH}:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games:/sbin:/usr/sbin:/bin:/usr/bin
 
 start_ros () { 
-    echo "ROS IP - ${ROS_IP}" > ${ROSBOTS_HOME}/roscore.log
-    echo "Starting roscore..." >> ${ROSBOTS_HOME}/roscore.log
-    nohup roscore >> ${ROSBOTS_HOME}/roscore.log 2>&1 & 
-
-    #mv /home/pi/driver_rpi.log /home/pi/driver_rpi.log.old
-    #nohup rosrun rosbots_driver driver_rpi.py >> /home/pi/driver_rpi.log 2>> /home/pi/driver_rpi.log &
+    nohup ${ROSBOTS_HOME}/rosbots_startup.sh > rosbots_startup.log 2>&1 &
 }
 
 stop_ros () {
